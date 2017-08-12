ef8da782aa5e060ce1bead4f5e3fc1f05098da1f
Paolo <gianpaolo1967@gmail.com>
2017-08-12 21:05:02 +0300

    added div and sizes

--- main.js
+++ main.js
@@ -10,3 +10,52 @@ document.getElementById('color').addEventListener('input', getColor, false);
       function getText() {
           document.getElementById('get_css').innerHTML = 'background: ' + color.value + ';' + '<br>' + 'color: ' + color.value + ';' + '<br>' + '';
 }
+
+var br = document.getElementById('br');
+var sizeHeight = document.getElementById('size');
+var sizeWidth = document.getElementById('size-w');
+var radius = document.getElementById('radius');
+var seeRadius = document.getElementById('see-radius');
+
+sizeHeight.addEventListener('input', getSize, false);
+
+function getSize(event) {
+  br.setAttribute ('style', '');
+  console.log (sizeHeight.value);
+  br.style.height = sizeHeight.value + 'px';
+  document.getElementById('height').value = 'height: ' + br.style.height;
+}
+
+sizeWidth.addEventListener('input', getSizeW, false);
+
+function getSizeW(event) {
+  console.log (sizeWidth.value);
+  br.style.width = sizeWidth.value + 'px';
+  document.getElementById('width').value = 'width: ' + br.style.width;
+}
+radius.addEventListener('input', getRadius, false);
+
+function getRadius(event) {
+  br.style.borderRadius = radius.value + 'px';
+  seeRadius.value = 'border-radius: ' + br.style.borderRadius;
+}
+
+// add value through input-text start!
+
+document.getElementById('height').addEventListener('input', typeHeight, false);
+function typeHeight(event) {
+  br.style.height = document.getElementById('height').value + 'px';
+}
+
+document.getElementById('width').addEventListener('input', typeWidth, false);
+function typeWidth(event) {
+  br.style.width = document.getElementById('width').value + 'px';
+}
+
+// pick color
+
+var pickColor = document.getElementById('pick-color').onclick = pickColorfromCp;
+
+function pickColorfromCp() {
+  br.style.background = colorValue.value;
+}
